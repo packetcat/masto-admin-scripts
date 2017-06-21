9f80e7fa3f7f5e1c5c43040c2d1e1b6980f516d2
Sadiq Saif <staticsafe@users.noreply.github.com>
2017-06-20 21:30:24 -0400

    Hardcode version number due to issues with previous method

--- mastodon-update.sh
+++ mastodon-update.sh
@@ -8,7 +8,7 @@ RAILS_ENV=production
 
 cd $MASTODON_INSTALL_DIR
 git pull
-git checkout $(git tag | tail -n 1)
+git checkout v1.4.4
 bundle install
 yarn install --pure-lockfile
 RAILS_ENV=production bundle exec rails db:migrate
