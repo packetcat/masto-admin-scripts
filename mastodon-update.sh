440626039bb5955b0e5f1589e20a55bdf1acdff3
Sadiq Saif <staticsafe@users.noreply.github.com>
2017-06-23 11:43:55 -0400

    Update Mastodon version to v1.4.6

--- mastodon-update.sh
+++ mastodon-update.sh
@@ -8,7 +8,7 @@ RAILS_ENV=production
 
 cd $MASTODON_INSTALL_DIR
 git pull
-git checkout v1.4.5
+git checkout v1.4.6
 bundle install
 yarn install --pure-lockfile
 RAILS_ENV=production bundle exec rails db:migrate
