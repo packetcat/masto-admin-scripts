2c7bd282fd82279ae754dd9a318a1507d5ff5153
Sadiq Saif <staticsafe@users.noreply.github.com>
2017-08-01 11:12:08 -0400

    Update Mastodon version in script v.1.5.0

--- mastodon-update.sh
+++ mastodon-update.sh
@@ -8,7 +8,7 @@ RAILS_ENV=production
 
 cd $MASTODON_INSTALL_DIR
 git pull
-git checkout v1.4.7
+git checkout v1.5.0
 bundle install
 yarn install --pure-lockfile
 RAILS_ENV=production bundle exec rails db:migrate
