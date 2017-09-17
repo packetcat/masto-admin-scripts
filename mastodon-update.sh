58c0a84fdd7903a360b3f89833780d9e21e15642
Sadiq Saif <staticsafe@users.noreply.github.com>
2017-09-17 15:35:13 -0400

    update git checkout command to automatically use latest

--- mastodon-update.sh
+++ mastodon-update.sh
@@ -8,7 +8,7 @@ RAILS_ENV=production
 
 cd $MASTODON_INSTALL_DIR
 git pull
-git checkout v1.6.0
+git checkout $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1)
 bundle install
 yarn install --pure-lockfile
 RAILS_ENV=production bundle exec rails db:migrate
