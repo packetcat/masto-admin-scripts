f0f44ab2254a26010744f335e39fc002299e9673
Sadiq Saif <staticsafe@users.noreply.github.com>
2018-10-21 21:26:12 -0400

    Specify path to bundle

--- mastodon-mediaremove.sh
+++ mastodon-mediaremove.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+/home/mastodon/.rbenv/shims/bundle
 cd /home/mastodon/live
-RAILS_ENV=production bundle exec /home/mastodon/live/bin/tootctl media remove
+RAILS_ENV=production /home/mastodon/.rbenv/shims/bundle exec /home/mastodon/live/bin/tootctl media remove
