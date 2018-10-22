da77ab5e5da627728574b030fe5fc41eb1a33e5a
Sadiq Saif <staticsafe@users.noreply.github.com>
2018-10-21 21:32:55 -0400

    Fix whatever I just did

--- mastodon-mediaremove.sh
+++ mastodon-mediaremove.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-/home/mastodon/.rbenv/shims/bundle
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
 cd /home/mastodon/live
 RAILS_ENV=production /home/mastodon/.rbenv/shims/bundle exec /home/mastodon/live/bin/tootctl media remove
